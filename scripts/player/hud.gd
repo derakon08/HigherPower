493f393624fda8d9985250ed9c094bb41adf769e
dera <derakon08@gmail.com>
2026-01-20 21:35:17 -0500

    Add signal to fix priority

--- scripts/player/hud.gd
+++ scripts/player/hud.gd
@@ -9,12 +9,14 @@ extends Control
 @export var hit_shake_strength : float
 @export_range(0.0, 1.0) var hit_shake_easing : float
 
-var _node_to_follow : Node
 var _pointers_size_halved : Vector2
+var _node_to_follow : Node
 var _screen_size : Vector2
 var _margin_size : Vector2
-var _shake : bool = false
 var _shake_value : Vector2
+var _shake : bool = false
+
+signal player_death
 
 func _ready():
     randomize()
@@ -79,16 +81,14 @@ func PlayerHit():
     _shake_value += hit_shake_value_max * hit_shake_strength
     _shake = true
     hurt_color.visible = true
+    hurt_color_timeout.start()
 
     if HP_bar.value <= 20:
         HP_bar.tint_progress = Color.RED
         hurt_color.color = Color.INDIAN_RED
 
     if HP_bar.value <= 0:
-        Main.GameEnd()
-    
-    else:
-        hurt_color_timeout.start()
+        player_death.emit()
 
 
 
