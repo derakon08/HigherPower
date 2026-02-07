f8b4b8f383fd4b869d584bfce92b7f51e874d189
dera <derakon08@gmail.com>
2026-02-06 22:49:11 -0500

    Adjust damage output for fast_wide

--- scripts/player/player_fast_wide.gd
+++ scripts/player/player_fast_wide.gd
@@ -2,7 +2,7 @@ extends "res://scripts/player/player_base.gd"
 @export var bullets_in_arc : int
 
 func _ModeOne():
-	if _to_be_fired > 1:
+	if (_to_be_fired > 1 && focus) || (_to_be_fired > 0.9 && !focus):
 		BulletMap.Shoot(
 			global_position + Vector2.UP * _aprox_radius,
 			bullet_speed,
@@ -18,7 +18,7 @@ func _ModeTwo():
 	var angle = deg_to_rad(-2 if focus else -30)
 	var angle_step = abs((angle * 2) / (bullets_in_arc))
 
-	if (!focus && _to_be_fired > bullets_in_arc) || (focus && _to_be_fired > bullets_in_arc + bullets_in_arc * 0.5):
+	if (!focus && _to_be_fired > bullets_in_arc) || (focus && _to_be_fired > bullets_in_arc + bullets_in_arc * 0.3):
 		for bullet in bullets_in_arc:
 			BulletMap.Shoot(
 				global_position + Vector2.UP * _aprox_radius,
