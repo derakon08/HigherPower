5485e034bce0df625fb91d78349186c8310c1bea
dera <derakon08@gmail.com>
2026-05-20 21:46:12 -0500

    Change player shooting style to omnidirectional
    Changes to the player_base include new input handling through a flag,
    Joypad motion events handling and the function _GetPlayerAim() -> float,
    which returns the angle for the direction of the player aim.
    
    Changes to player_fast_wide are just adjustements to use this new
    function.

--- scripts/player/player_base.gd
+++ scripts/player/player_base.gd
@@ -1,11 +1,10 @@
 extends Area2D
 @export var invinsibility_timer : Timer
 @export var bomb_cooldown_timer : Timer
+@export_range(0.0, 1.0) var GAMEPAD_DEADZONE : float = 0.03
 @export var normal_speed : int = 500
 @export var focus_speed : int = 100
 @export var fire_rate : float = 1000
-@export var bullet_speed : float = 2000
-@export var bullet_distance : float = 2
 
 ##This's been exported since i NEED the setter to activate
 @export var shoot_mode : bool:
@@ -19,8 +18,8 @@ extends Area2D
 	get:
 		return _shoot_mode
 
-var focus = false
-var allow_shooting : bool = true
+var _focus = false
+var _allow_shooting : bool = false
 
 var _Shoot : Callable = Callable(_ModeOne)
 var _shoot_mode : bool
@@ -39,67 +38,109 @@ var _aprox_radius = 20
 
 signal player_hit
 
+
+
+
 func _ready() -> void:
 	_particle_explosion = $ParticlesExplosion
 	_transition_node = $Transition
 	_transition_node.AllowMovement()
 
 	_transition_node.end_of_pool.connect(_BombEnd)
-	invinsibility_timer.timeout.connect(Vulnerable.bind(true))
+	invinsibility_timer.timeout.connect(_Vulnerable.bind(true))
 	bomb_cooldown_timer.timeout.connect(BombReady.bind(true))
+
 
 func _physics_process(delta: float) -> void:
 	var velocity : Vector2 = Input.get_vector("left", "right", "up", "down")
-	focus = Input.is_action_pressed("focus")
 
-	position += velocity * (focus_speed if focus else normal_speed) * delta
+	position += velocity * (focus_speed if _focus else normal_speed) * delta
+
 
 func _process(delta: float) -> void:
-	if (_bomb_ready && Input.is_action_just_pressed("ship_action")):
-		_BombStart()
-	elif (allow_shooting && Input.is_action_pressed("shoot") && _vulnerable):
+	if (_allow_shooting && _vulnerable):
 		_to_be_fired += fire_rate * delta
 		_Shoot.call()
-	else:
-		_to_be_fired = 0
+
+
+func _input(event : InputEvent) -> void:
+	if event is InputEventJoypadMotion:
+		if event.is_action("shoot"):
+			_allow_shooting = event.axis_value > 0.3
+
+		elif event.is_action("focus"):
+			_focus = event.axis_value > 0.5
+		
+		elif _bomb_ready && event.is_action("ship_action") && event.axis_value > 0.4:
+			_BombStart()
+		
+		return
+
+	if event.is_action("shoot"):
+		_allow_shooting = event.pressed
+
+	elif event.is_action("focus"):
+		_focus = event.pressed
+	
+	elif _bomb_ready && event.is_action("ship_action"):
+		_BombStart()
+
 
 func _BombStart():
-	Vulnerable(false)
+	_Vulnerable(false)
 	_bomb_ready = false
 	shoot_mode = !_shoot_mode
-
 	BulletMap.ClearGameBullets()
 
 	Main.warp.emit()
 	bomb_cooldown_timer.start()
 	_transition_node.AllowShooting()
 
+
 func _BombEnd():
 	Main.InvertBackgroundColor()
 	BulletMap.NukeGameBullets()
 
+
+func _GetPlayerAim() -> float:
+	var strength : Vector2 = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
+
+	if strength.length() > GAMEPAD_DEADZONE:
+		return strength.angle()
+	
+	return (get_viewport().get_mouse_position() - global_position).normalized().angle()
+
+
+
+
+
 func Hit(_args):
 	if _vulnerable:
-		Vulnerable(false)
+		_Vulnerable(false)
 		_particle_explosion.emitting = true
 		player_hit.emit()
 
-func Switch(on : bool):
-	self.set_physics_process(on)
-	self.set_process(on)
-	self.visible = on
 
-func Vulnerable(on : bool):
+func Switch(on : bool):
+	set_physics_process(on)
+	set_process(on)
+	set_process_input(on)
+	visible = on
+
+
+func _Vulnerable(on : bool):
 	if !on:
 		invinsibility_timer.start()
 
 	_vulnerable = on
 
+
 func BombReady(on = true):
 	if on && !Main.narrator.IsTalking():
-		Main.narrator.Talk("Warp ready.")
+		Main.narrator.Talk("Get me out of here")
 	
 	_bomb_ready = on
+	bomb_cooldown_timer.stop()
 
 
 
--- scripts/player/player_fast_wide.gd
+++ scripts/player/player_fast_wide.gd
@@ -1,13 +1,17 @@
 extends "res://scripts/player/player_base.gd"
+@export var bullet_speed : float = 2000
+@export var bullet_distance : float = 2
 @export var bullets_in_arc : int
 
 func _ModeOne():
-	if (_to_be_fired > 1 && focus) || (_to_be_fired > 0.9 && !focus):
+	if (_to_be_fired > 1 && _focus) || (_to_be_fired > 0.9 && !_focus):
+		var aim : float = _GetPlayerAim()
+
 		BulletMap.Shoot(
-			global_position + Vector2.UP * _aprox_radius,
+			global_position + Vector2.RIGHT.rotated(aim) * _aprox_radius,
 			bullet_speed,
 			bullet_distance, 
-			_account_for_rotation if focus else _account_for_rotation + randf_range(-20,20) * 0.01745,
+			aim if _focus else aim + randf_range(-20,20) * 0.01745,
 			50,
 			"enemies",
 			_atlas_sprite)
@@ -15,16 +19,17 @@ func _ModeOne():
 		_to_be_fired = 0
 
 func _ModeTwo():
-	var angle = deg_to_rad(-2 if focus else -30)
-	var angle_step = abs((angle * 2) / (bullets_in_arc))
+	if (!_focus && _to_be_fired > bullets_in_arc) || (_focus && _to_be_fired > bullets_in_arc + bullets_in_arc * 0.3):
+		var angle = deg_to_rad(-2 if _focus else -30)
+		var angle_step = abs((angle * 2) / (bullets_in_arc))
+		var aim : float = _GetPlayerAim()
 
-	if (!focus && _to_be_fired > bullets_in_arc) || (focus && _to_be_fired > bullets_in_arc + bullets_in_arc * 0.3):
 		for bullet in bullets_in_arc:
 			BulletMap.Shoot(
-				global_position + Vector2.UP * _aprox_radius,
+				global_position + Vector2.RIGHT.rotated(aim + angle) * _aprox_radius,
 				bullet_speed,
 				bullet_distance, 
-				_account_for_rotation + angle,
+				aim + angle,
 				50,
 				"enemies",
 				_atlas_sprite)
